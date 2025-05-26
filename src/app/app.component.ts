efd693aaf8640ad795c7a3c6a6fc9eb840458b43
ptitsynamv <ptitsynamv@gmail.com>
2025-05-26 11:21:20 +0300

    Add layout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavComponent } from './layout/components/nav/nav.component';
+import { HeaderComponent } from './layout/components/header/header.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NavComponent, HeaderComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'mouse-app';
