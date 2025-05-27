2bc3d3859f859be14db63881ce26e1e6b1a12dff
ptitsynamv <ptitsynamv@gmail.com>
2025-05-27 10:30:06 +0300

    Add signal

--- src/app/layout/components/nav/nav.component.ts
+++ src/app/layout/components/nav/nav.component.ts
@@ -1,11 +1,16 @@
 import { Component } from '@angular/core';
+import { signal } from '@angular/core';
 
 @Component({
   selector: 'app-nav',
   imports: [],
   templateUrl: './nav.component.html',
-  styleUrl: './nav.component.scss'
+  styleUrl: './nav.component.scss',
 })
 export class NavComponent {
+  public isMenuOpen = signal(false);
 
+  public menuToggle() {
+    this.isMenuOpen.set(!this.isMenuOpen());
+  }
 }
